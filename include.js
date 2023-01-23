355a71d65a00f0d96e3e16ae2101a194fcab0c99
fabianou <fabianouvina3@gmail.com>
2023-01-23 14:56:07 -0300

    trabajo 1

---
+++ include.js
@@ -0,0 +1,19 @@
+const valoresBase = [1,2,3,4,5,6];
+                     0,1,2,3,4,5
+
+const nuevosValores = valoresBase.map((numero, indice) => numero**indice);
+
+console.log(nuevosValores);
+
+const nombres = ['Alex', 'Camilo','Nicolas', 'Mariano'];
+
+if (nombres.includes('Camilo')) {
+console.log('Si esta dentro del arreglo');
+}else {
+    console.log('No')
+}
+
+
+
+
+
